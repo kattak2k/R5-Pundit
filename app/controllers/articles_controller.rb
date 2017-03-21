fa5552e475f0fc514b8b0465458b2e1dbb70a2a7
kattak2k <kkiran.ror@gmail.com>
2017-03-21 20:12:22 +0530

    create articles policy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
+    authorize @articles
   end
 
   # GET /articles/1
@@ -15,6 +16,7 @@ class ArticlesController < ApplicationController
   # GET /articles/new
   def new
     @article = Article.new
+    authorize @articles
   end
 
   # GET /articles/1/edit
@@ -25,6 +27,8 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
+    @article.user = current_user  
+    authorize @article
 
     respond_to do |format|
       if @article.save
@@ -65,6 +69,7 @@ class ArticlesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
+      authorize @article
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -2,21 +2,21 @@ class ArticlePolicy < ApplicationPolicy
 
 before_action :status_return, only: [:update, :destroy]
 
-def index?
+    def index?
     true
-end    
-def create?
-  user.present?
-end
-def update?
-end
-def destroy?
-end
+    end    
+    def create?
+    user.present?
+    end
+    def update?
+    end
+    def destroy?
+    end
 
-private
-def article
-record     
-end
+    private
+        def article
+        record     
+        end
 
 def status_return
    return true if user.present? && user == article.user 
